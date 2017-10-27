dc88807b6e412b86f90c98a9a30c64478a71e318
guleatoma <toma@turo.com>
2017-10-27 14:51:23 +0200

    change path for raw font files

--- R/import_turo_fonts.R
+++ R/import_turo_fonts.R
@@ -17,12 +17,13 @@ import_turo_fonts <- function() {
     for (i in 1:length(font_names)) {
         test_file = paste0(sysfonts::font.paths()[2], "/", font_names[i])
         if (!file.exists(test_file)) {
-            origin = paste0("https://github.com/guleatoma/ggtech/raw/master/Fonts/", 
+            origin = paste0("https://raw.githubusercontent.com/guleatoma/ggtech/master/Fonts/", 
                 font_names[i])
             download.file(origin, test_file, method = "curl")
         }
         extrafont::font_import(pattern = font_names[i], prompt = FALSE)
     }
 }
+
 
 
